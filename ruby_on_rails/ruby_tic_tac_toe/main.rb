eae432964d5ca7c1aff6514636877abe49e611c2
dwlknsn <dwlknsn@users.noreply.github.com>
2024-09-10 15:10:05 +0700

    remove old folders

--- ruby_on_rails/ruby_tic_tac_toe/main.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'tic_tac_toe'
-
-player1 = TicTacToe::Player.new("Player1")
-player2 = TicTacToe::Player.new("Player2")
-
-game = TicTacToe::Game.new([player1, player2])
-
-game.start
