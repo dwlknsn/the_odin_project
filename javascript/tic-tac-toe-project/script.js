68d9bcd7a57291bfca019bdf56d6923e807b975f
dwlknsn <dwlknsn@users.noreply.github.com>
2025-01-07 10:43:47 +0700

    Adds function to create the board and place tokens

--- javascript/tic-tac-toe-project/script.js
+++ javascript/tic-tac-toe-project/script.js
@@ -1,1 +1,28 @@
 console.log("connected");
+
+function createGameboard() {
+  const rows = 3;
+  const cols = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board.push([])
+    for (let j = 0; j < cols; j++) {
+      board[i].push(1)
+    }
+
+  }
+
+  const getBoard = () => board;
+  const placeToken = (token, [x, y]) => {
+    board[x][y] = token;
+  }
+
+  return { getBoard, placeToken }
+}
+
+const board = createGameboard();
+board.placeToken("X", [2, 1]);
+board.placeToken("X", [0, 1]);
+board.placeToken("X", [1, 0]);
+console.table(board.getBoard());
